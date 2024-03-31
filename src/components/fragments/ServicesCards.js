72058a22327ac27e5b177f749a241f751aaa1830
BuildTools <kopcsanyiludovit@gmail.com>
2024-03-31 15:10:09 +0200

    fixing responsiveness

--- src/components/fragments/ServicesCards.js
+++ src/components/fragments/ServicesCards.js
@@ -37,7 +37,7 @@ const ServicesCards = () => {
                 }}
                 className={ mainCard.className }
             />
-            <div className='grid grid-cols-3'>
+            <div className='grid grid-cols-1 sm:grid-cols-3'>
                 {
                     smallCards.map((card, i) => {
                         return <InfoCard
